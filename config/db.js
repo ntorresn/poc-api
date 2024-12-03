b8cab4560312e13e854f0239260c153b97ccf6b7
nTorres <nelson.torresperlaza@externos.bci.cl>
2024-12-03 14:58:30 -0500

    Host

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://75.119.156.86:36017/poc-products-db?authSource=admin', {
+mongoose.connect('mongodb://127.0.0.1:36017/poc-products-db?authSource=admin', {
     'auth': { 'authSource': 'admin' },
     'user': 'ntorres',
     'pass': 'nTorres.12',
